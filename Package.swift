d878f0b77a5d2069e347f6795461566798a46169
Sanjay Mali <malisanjay5433@users.noreply.github.com>
2021-05-28 13:18:38 +0530

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,23 @@
+import PackageDescription
+let package = Package(
+    name: "SVProgressHUD",
+    platforms: [
+        .iOS(.v12),
+    ],
+    products: [
+        .library(
+            name: "SVProgressHUD",
+            targets: ["SVProgressHUD"]),
+    ],
+    dependencies: [
+        // no dependencies
+    ],
+    targets: [
+        .target(
+            name: "SVProgressHUD",
+            path: "./Sources"),
+        .testTarget(
+            name: "SVProgressHUDTests",
+            dependencies: ["SVProgressHUD"]),
+    ]
+)
